a23cf945a2a09e07748309541dd19629c1571cc4
jahandar00 <axiyev.cahandar@gmail.com>
2023-10-14 00:25:07 +0200

    first commit

---
+++ scrirpts/forecast.js
@@ -0,0 +1,31 @@
+const key = 'MxAsPZCbJ4XWOgiDs17oWgSU4W3EaJD0';
+
+//get weather info
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+
+//get city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+
+
